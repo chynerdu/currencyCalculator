19c0653185f3fffbf2e454dcfa8ffb5692d27a49
chinedu <chinedu.uche@thebulb.africa>
2022-02-24 11:39:35 +0100

    Implementing new conversion API

--- lib/services/api_path.dart
+++ lib/services/api_path.dart
@@ -4,6 +4,9 @@ class APIPath {
   static String converter(query) =>
       '${Config.baseUrl}/api/v7/convert?q=$query&compact=ultra&apiKey=${Config.apiKey}';
 
+  // static String converter(from, to, amount) =>
+  //     '${Config.fixerBaseUrl}/convert?access_key=${Config.apiKey}&from=$from&to=$to&amount=&amount';
+
   static String currencies() =>
       '${Config.baseUrl}/api/v7/currencies?apiKey=${Config.apiKey}';
 }
--- lib/services/config.dart
+++ lib/services/config.dart
@@ -1,4 +1,6 @@
 class Config {
+  static final fixerBaseUrl = "https://data.fixer.io/api";
+  static const fixerAPIKey = "2914c1f8420d47e1b7a30cc99e767694";
   static final apiKey = 'c1699be90b9b0f7f724f';
   static final baseUrl = 'https://free.currconv.com';
 }
